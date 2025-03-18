5c93e3372b41f0026fb04e25b84447c05aa75466
Barder_Lucas <bardey.estelle1@gmail.com>
2025-03-18 09:41:36 +0100

    Version initial non personalisé

---
+++ src/backend.ts
@@ -0,0 +1,4 @@
+import PocketBase  from 'pocketbase'
+import type { TypedPocketBase } from './pocketbase-typegen';
+export const pb = new PocketBase(import.meta.env.PUBLIC_POKEAPI) as TypedPocketBase;
+
