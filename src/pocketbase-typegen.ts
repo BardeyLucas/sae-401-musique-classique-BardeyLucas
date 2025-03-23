04ed10c42905ce3dc9940f66eaf8d9a28661a28e
Barder_Lucas <bardey.estelle1@gmail.com>
2025-03-23 16:36:27 +0100

    Réparation de la base de donnée

--- src/pocketbase-typegen.ts
+++ src/pocketbase-typegen.ts
@@ -6,14 +6,21 @@ import type PocketBase from 'pocketbase'
 import type { RecordService } from 'pocketbase'
 
 export enum Collections {
+	Compositeur = "Compositeur",
+	Formation = "Formation",
+	ImageHero = "ImageHero",
+	ImagesSlider = "ImagesSlider",
+	Interpretation = "Interpretation",
+	Oeuvre = "Oeuvre",
+	Offre = "Offre",
+	PageContent = "Page_content",
+	Platforme = "Platforme",
+	Projets = "Projets",
 	Authorigins = "_authOrigins",
 	Externalauths = "_externalAuths",
 	Mfas = "_mfas",
 	Otps = "_otps",
 	Superusers = "_superusers",
-	Films = "films",
-	Personnes = "personnes",
-	Roles = "roles",
 	Users = "users",
 }
 
@@ -38,6 +45,46 @@ export type AuthSystemFields<T = never> = {
 } & BaseSystemFields<T>
 
 // Record types for each collection
+
+export type CompositeurRecord = {
+	id: string
+}
+
+export type FormationRecord = {
+	id: string
+}
+
+export type ImageHeroRecord = {
+	id: string
+}
+
+export type ImagesSliderRecord = {
+	id: string
+}
+
+export type InterpretationRecord = {
+	id: string
+}
+
+export type OeuvreRecord = {
+	id: string
+}
+
+export type OffreRecord = {
+	id: string
+}
+
+export type PageContentRecord = {
+	id: string
+}
+
+export type PlatformeRecord = {
+	id: string
+}
+
+export type ProjetsRecord = {
+	id: string
+}
 
 export type AuthoriginsRecord = {
 	collectionRef: string
@@ -88,101 +135,70 @@ export type SuperusersRecord = {
 	verified?: boolean
 }
 
-export type FilmsRecord = {
-	created?: IsoDateString
-	dateSortie?: IsoDateString
-	duree?: number
-	id: string
-	langue?: string
-	producteur?: RecordIdString
-	role?: RecordIdString[]
-	scenaristes?: RecordIdString[]
-	synopsis?: HTMLString
-	titre?: string
-	updated?: IsoDateString
-}
-
-export enum PersonnesNationaliteOptions {
-	"FR" = "FR",
-	"UK" = "UK",
-	"US" = "US",
-}
-
-export enum PersonnesProfessionOptions {
-	"acteur" = "acteur",
-	"réalisateur" = "réalisateur",
-	"producteur" = "producteur",
-	"scénariste" = "scénariste",
-}
-export type PersonnesRecord = {
-	created?: IsoDateString
-	dateDeces?: IsoDateString
-	dateNaissance?: IsoDateString
-	id: string
-	lieuDeces?: string
-	lieuNaissance?: string
-	nationalite?: PersonnesNationaliteOptions
-	nom?: string
-	prenom?: string
-	profession?: PersonnesProfessionOptions[]
-	updated?: IsoDateString
-}
-
-export type RolesRecord = {
-	acteur?: RecordIdString
-	created?: IsoDateString
-	id: string
-	nomRole?: string
-	updated?: IsoDateString
-}
-
 export type UsersRecord = {
-	avatar?: string
-	created?: IsoDateString
 	email: string
 	emailVisibility?: boolean
 	id: string
-	name?: string
 	password: string
 	tokenKey: string
-	updated?: IsoDateString
 	verified?: boolean
 }
 
 // Response types include system fields and match responses from the PocketBase API
+export type CompositeurResponse<Texpand = unknown> = Required<CompositeurRecord> & BaseSystemFields<Texpand>
+export type FormationResponse<Texpand = unknown> = Required<FormationRecord> & BaseSystemFields<Texpand>
+export type ImageHeroResponse<Texpand = unknown> = Required<ImageHeroRecord> & BaseSystemFields<Texpand>
+export type ImagesSliderResponse<Texpand = unknown> = Required<ImagesSliderRecord> & BaseSystemFields<Texpand>
+export type InterpretationResponse<Texpand = unknown> = Required<InterpretationRecord> & BaseSystemFields<Texpand>
+export type OeuvreResponse<Texpand = unknown> = Required<OeuvreRecord> & BaseSystemFields<Texpand>
+export type OffreResponse<Texpand = unknown> = Required<OffreRecord> & BaseSystemFields<Texpand>
+export type PageContentResponse<Texpand = unknown> = Required<PageContentRecord> & BaseSystemFields<Texpand>
+export type PlatformeResponse<Texpand = unknown> = Required<PlatformeRecord> & BaseSystemFields<Texpand>
+export type ProjetsResponse<Texpand = unknown> = Required<ProjetsRecord> & BaseSystemFields<Texpand>
 export type AuthoriginsResponse<Texpand = unknown> = Required<AuthoriginsRecord> & BaseSystemFields<Texpand>
 export type ExternalauthsResponse<Texpand = unknown> = Required<ExternalauthsRecord> & BaseSystemFields<Texpand>
 export type MfasResponse<Texpand = unknown> = Required<MfasRecord> & BaseSystemFields<Texpand>
 export type OtpsResponse<Texpand = unknown> = Required<OtpsRecord> & BaseSystemFields<Texpand>
 export type SuperusersResponse<Texpand = unknown> = Required<SuperusersRecord> & AuthSystemFields<Texpand>
-export type FilmsResponse<Texpand = unknown> = Required<FilmsRecord> & BaseSystemFields<Texpand>
-export type PersonnesResponse<Texpand = unknown> = Required<PersonnesRecord> & BaseSystemFields<Texpand>
-export type RolesResponse<Texpand = unknown> = Required<RolesRecord> & BaseSystemFields<Texpand>
 export type UsersResponse<Texpand = unknown> = Required<UsersRecord> & AuthSystemFields<Texpand>
 
 // Types containing all Records and Responses, useful for creating typing helper functions
 
 export type CollectionRecords = {
+	Compositeur: CompositeurRecord
+	Formation: FormationRecord
+	ImageHero: ImageHeroRecord
+	ImagesSlider: ImagesSliderRecord
+	Interpretation: InterpretationRecord
+	Oeuvre: OeuvreRecord
+	Offre: OffreRecord
+	Page_content: PageContentRecord
+	Platforme: PlatformeRecord
+	Projets: ProjetsRecord
 	_authOrigins: AuthoriginsRecord
 	_externalAuths: ExternalauthsRecord
 	_mfas: MfasRecord
 	_otps: OtpsRecord
 	_superusers: SuperusersRecord
-	films: FilmsRecord
-	personnes: PersonnesRecord
-	roles: RolesRecord
 	users: UsersRecord
 }
 
 export type CollectionResponses = {
+	Compositeur: CompositeurResponse
+	Formation: FormationResponse
+	ImageHero: ImageHeroResponse
+	ImagesSlider: ImagesSliderResponse
+	Interpretation: InterpretationResponse
+	Oeuvre: OeuvreResponse
+	Offre: OffreResponse
+	Page_content: PageContentResponse
+	Platforme: PlatformeResponse
+	Projets: ProjetsResponse
 	_authOrigins: AuthoriginsResponse
 	_externalAuths: ExternalauthsResponse
 	_mfas: MfasResponse
 	_otps: OtpsResponse
 	_superusers: SuperusersResponse
-	films: FilmsResponse
-	personnes: PersonnesResponse
-	roles: RolesResponse
 	users: UsersResponse
 }
 
@@ -190,13 +206,20 @@ export type CollectionResponses = {
 // https://github.com/pocketbase/js-sdk#specify-typescript-definitions
 
 export type TypedPocketBase = PocketBase & {
+	collection(idOrName: 'Compositeur'): RecordService<CompositeurResponse>
+	collection(idOrName: 'Formation'): RecordService<FormationResponse>
+	collection(idOrName: 'ImageHero'): RecordService<ImageHeroResponse>
+	collection(idOrName: 'ImagesSlider'): RecordService<ImagesSliderResponse>
+	collection(idOrName: 'Interpretation'): RecordService<InterpretationResponse>
+	collection(idOrName: 'Oeuvre'): RecordService<OeuvreResponse>
+	collection(idOrName: 'Offre'): RecordService<OffreResponse>
+	collection(idOrName: 'Page_content'): RecordService<PageContentResponse>
+	collection(idOrName: 'Platforme'): RecordService<PlatformeResponse>
+	collection(idOrName: 'Projets'): RecordService<ProjetsResponse>
 	collection(idOrName: '_authOrigins'): RecordService<AuthoriginsResponse>
 	collection(idOrName: '_externalAuths'): RecordService<ExternalauthsResponse>
 	collection(idOrName: '_mfas'): RecordService<MfasResponse>
 	collection(idOrName: '_otps'): RecordService<OtpsResponse>
 	collection(idOrName: '_superusers'): RecordService<SuperusersResponse>
-	collection(idOrName: 'films'): RecordService<FilmsResponse>
-	collection(idOrName: 'personnes'): RecordService<PersonnesResponse>
-	collection(idOrName: 'roles'): RecordService<RolesResponse>
 	collection(idOrName: 'users'): RecordService<UsersResponse>
 }
